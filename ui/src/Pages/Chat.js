ff6aeeb4b48bfbb76067a971bfc22fefe1180485
ravishnu17 <ravinshnu@dbcyelagiri.edu.in>
2023-10-03 10:59:03 +0530

    new icon

--- ui/src/Pages/Chat.js
+++ ui/src/Pages/Chat.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import typing_gif from '../Assets/typing.gif';
 import '../Style/style.css';
-import sendIcon from '../Assets/send.png';
+import sendIcon from '../Assets/send.gif';
 
 function Chat() {
   const location = useLocation();
@@ -190,11 +190,8 @@ function Chat() {
           <input className='form-control border-secondary p-2 mx-2' autoComplete='off'
             placeholder='Message here'
             {...register('msg', { required: true, onChange: (e) => { typing(true, e.target.value) }, onBlur: () => { typing(false) } })} />
-          <button className=' btn' type='submit' title='Send'>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-            </svg> */}
-            <img src={sendIcon} width={50} />
+          <button className='btn' type='submit' title='Send'>
+            <img src={sendIcon} width={40} />
           </button>
         </form>
       </div>
