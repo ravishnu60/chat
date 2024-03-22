75f3018c788c0beb8f15e9048e43a3f47acd0f68
ravishnu60 <ravishnu60@gmail.com>
2024-03-22 19:34:49 +0530

    design

--- ui/src/Pages/Login.js
+++ ui/src/Pages/Login.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
-import { alert, base_url, loadingFunc } from '../Utils/Utility';
-import { useNavigate } from 'react-router-dom';
+import { alert, base_url, isMobile, loadingFunc } from '../Utils/Utility';
+import { Link, useNavigate } from 'react-router-dom';
 import '../Style/style.css';
 import logo from '../Assets/logo.png'
+import bg from '../Assets/bg.png'
+import login from '../Assets/login_side.jpg'
 
 function Login() {
   const [error, setError] = useState(false);
@@ -13,7 +15,7 @@ function Login() {
   const { register: reg, handleSubmit: regSubmit, formState: { errors: regError }, reset: regReset } = useForm();
   const navigate = useNavigate();
   const [view, setView] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); 
 
   const submit = (data) => {
     setLoading(true);
@@ -65,87 +67,93 @@ function Login() {
   useEffect(() => {
     sessionStorage.clear();
   }, [])
-  
-  
+
+
   return (
     <div>
+      <img src={bg} className='login_bg' alt='Background' />
       <div className='fixed-top'>
-        <div className='text-center p-2 d-flex justify-content-between align-items-center header'>
-          <div> </div>
-          <div className=' h5 fw-bold'>Connect <img src={logo} width={25} alt='Logo'/></div>
-          <div> </div>
+        <div className='p-2 text-center header'>
+          <div className='h4' style={{ fontWeight: 'bolder' }}>Connect <img src={logo} width={25} alt='Logo' /></div>
         </div>
       </div>
-      <div className='p-5'>
-        {loadingFunc(loading, step==0)}
-        <div className="row justify-content-center align-items-center mt-5">
-          <div className="col-lg-4 col-11 text-light">
-            <form className={error ? 'border border-danger rounded p-3' : 'border border-success rounded p-3'} onSubmit={step ? regSubmit(registerNew) : handleSubmit(submit)}>
-              {error && <div className="text-danger text-center h5">Invalid credentials</div>}
-              <h5 className='bg-info p-2 text-light text-center'>{step ? "Create Account" : "Login to your Account"}</h5>
-              {step === 0 ?
-                <>
-                  <div className="form mb-4">
-                    <label className="form-label" >Mobile No.</label>
-                    <input type="number"
-                      className="form-control"
-                      {...register('username', { required: true })} onFocus={() => { setError(false) }} />
-                    {errors?.username && <div className="text-danger">Mobile number is required</div>}
-                  </div>
 
-                  <div className="form-outline mb-4">
-                    <label className="form-label" >Password</label>
-                    <div className='input-group'>
-                      <input type={view ? "text" : "password"} autoComplete='off' className="form-control border-right-0"
-                        {...register('password', { required: true })} onFocus={() => { setError(false) }} />
-                      <div className='input-group-text' style={{ cursor: 'pointer' }} onClick={() => { setView(!view) }}>{view ? <i className='fa fa-eye'></i> : <i className='fa fa-eye-slash'></i>}</div>
+      {loadingFunc(loading, step === 0)}
+
+      <div className={isMobile ? 'down-form':"center"}>
+        <div className="login_form" style={{ borderColor: error ? 'red' : 'white' }}>
+          <form className='row' onSubmit={step ? regSubmit(registerNew) : handleSubmit(submit)}>
+            <div className='col-lg-6 col-0'>
+              <img src={login} style={{width:'inherit'}} alt='side' />
+            </div>
+            <div className='col-lg-6 col-12'>
+              <div className='row'>
+                {error && <div className="text-danger text-center h5 col-12 font-weight-bold">Invalid credentials</div>}
+                <h5 className='text-light text-center col-12 login_title'>{step ? "Create Account" : "Login"}</h5>
+                {step === 0 ?
+                  <>
+                    <div className="col-12 mb-4 login__inputs">
+                      <input type="text"
+                        className="form-control login__box"
+                        placeholder='Mobile No.'
+                        {...register('username', { required: true })}
+                        onFocus={() => { setError(false) }}
+                      />
+                      {errors?.username && <div className="text-danger font-weight-bold">Mobile number is required</div>}
                     </div>
-                    {errors?.password && <div className="text-danger">Password is required</div>}
-                  </div>
-                </>
-                :
-                <>
-                  <div className="form mb-4">
-                    <label className="form-label" >Name</label>
-                    <input type="text"
-                      className="form-control"
-                      {...reg('name', { required: true })} onFocus={() => { setError(false) }} />
-                    {regError?.name && <div className="text-danger">Name is required</div>}
-                  </div>
 
-                  <div className="form mb-4">
-                    <label className="form-label" >Mobile No.</label>
-                    <input type="number"
-                      className="form-control"
-                      {...reg('phone_no', { required: true })} onFocus={() => { setError(false) }} />
-                    {regError?.phone_no && <div className="text-danger">Mobile number is required</div>}
-                  </div>
-
-                  <div className="form-outline mb-4">
-                    <label className="form-label" >Password</label>
-                    <div className='input-group'>
-                      <input type={view ? "text" : "password"} autoComplete='off' className="form-control border-right-0"
-                        {...reg('password', { required: true })} onFocus={() => { setError(false) }} />
-                      <div className='input-group-text' style={{ cursor: 'pointer' }} onClick={() => { setView(!view) }}>{view ? <i className='fa fa-eye'></i> : <i className='fa fa-eye-slash'></i>}</div>
+                    <div className="col-12 mb-3">
+                      <div className='input-group'>
+                        <input 
+                          type={view ? "text" : "password"} 
+                          autoComplete='off' 
+                          className="form-control border-right-0"
+                          placeholder='Password'
+                          {...register('password', { required: true })} onFocus={() => { setError(false) }} />
+                        <div className='input-group-text' style={{ cursor: 'pointer' }} onClick={() => { setView(!view) }}>{view ? <i className='fa fa-eye text-light'></i> : <i className='fa fa-eye-slash text-light  '></i>}</div>
+                      </div>
+                      {errors?.password && <div className="text-danger font-weight-bold">Password is required</div>}
                     </div>
-                    {regError?.password && <div className="text-danger">Password is required</div>}
-                  </div>
-                </>
-              }
+                  </>
+                  :
+                  <>
+                    <div className="form mb-4">
+                      <label className="form-label" >Name</label>
+                      <input type="text"
+                        className="form-control"
+                        {...reg('name', { required: true })} onFocus={() => { setError(false) }} />
+                      {regError?.name && <div className="text-danger">Name is required</div>}
+                    </div>
 
+                    <div className="form mb-4">
+                      <label className="form-label" >Mobile No.</label>
+                      <input type="number"
+                        className="form-control"
+                        {...reg('phone_no', { required: true })} onFocus={() => { setError(false) }} />
+                      {regError?.phone_no && <div className="text-danger">Mobile number is required</div>}
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <label className="form-label" >Password</label>
+                      <div className='input-group'>
+                        <input type={view ? "text" : "password"} autoComplete='off' className="form-control border-right-0"
+                          {...reg('password', { required: true })} onFocus={() => { setError(false) }} />
+                        <div className='input-group-text' style={{ cursor: 'pointer' }} onClick={() => { setView(!view) }}>{view ? <i className='fa fa-eye text-light'></i> : <i className='fa fa-eye-slash text-light'></i>}</div>
+                      </div>
+                      {regError?.password && <div className="text-danger">Password is required</div>}
+                    </div>
+                  </>
+                }
+              </div>
               <div className="d-flex justify-content-around align-items-center">
-
-                {/* <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-                    <label className="form-check-label" htmlFor="form1Example3"> Remember me </label>
-                  </div> */}
                 {/* <a href="#!">Forgot password?</a> */}
               </div>
 
-              <button type="submit" className="btn btn-success  btn-block">{step ? "Sign up" : "Login"}</button>
-              <button type="button" onClick={handlePage} className="btn btn-primary  btn-block">{step ? "Back" : "Sign up"}</button>
-            </form>
-          </div>
+              <button type="submit" className="btn btn-light btn-block login-button">{step ? "Sign up" : "Login"}</button>
+              <h6 className='text-light text-center mt-3'>Don't have an account? <Link className='font-weight-bold text-light text-nowrap' onClick={handlePage}>Sign up!</Link></h6>
+              { step ? <button type="button" onClick={handlePage} className='btn btn-dark btn-block login-button'>Back</button> : null}
+            </div>
+          </form>
         </div>
       </div>
     </div>
